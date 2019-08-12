0069037cdb0f112a0f007fa350b6aa93d10fe0ae
rachid <olgor@hotmail.com>
2019-08-12 21:15:35 +0200

    logManager added

---
+++ src/LogManager.php
@@ -0,0 +1,27 @@
+<?php
+namespace App;
+use Monolog\Logger;
+use Monologe\Handler\StreamHandler;
+
+class LogManager{
+    private $logger;
+    public function __construct(){
+        $this->logger = new logger("App");
+        $this->logger->pushHandler(new StreamHandler(dirname(__DIR__).'/cache/logs/dev.log'));
+
+    }
+    public function info(string $message){
+        $this->logger->info($message);
+    }
+    public function warning(string $message){
+        $this->logger->warning($message);
+    }
+    public function error(string $message){
+        $this->logger->error($message);
+    }
+    public function getLogger():Logger{
+        return $this->logger;
+    }
+
+
+}
--- src/kernel.php
+++ src/kernel.php
@@ -2,6 +2,8 @@
 namespace App;
 class Kernel{
     public function __construct(){
+        $logManager = new LogManager();
+        $logManager->info("Arrancando Aplicacion");
         $viewManager = new ViewManager();
        $viewManager->renderTemplate("index.twig.html");
         
